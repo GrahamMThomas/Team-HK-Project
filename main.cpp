5b1eb461ae3ddb9d372414db9aa62aea69e08b6a
Graham Thomas <grahamthethomas@gmail.com>
2016-09-17 13:01:41 -0700

    Mergeing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include "mainwindow.h"
-<<<<<<< HEAD
-=======
 #include "qtftp/src/qftp/qftp.h"
 #include "qtftp/src/qftp/qurlinfo.h"
 
 #include <QObject>
->>>>>>> 30b3f4576b6fd7563c04134f6950e6e6b6baefe2
 #include <QApplication>
 #include <QDebug>
 
